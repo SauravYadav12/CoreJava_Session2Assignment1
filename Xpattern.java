c9437eca1a9dd57fbabbb276a3ea9cd3e81c79a3
Saurav Yadav <sauravyadav1286@gmail.com>
2017-03-31 08:02:18 +0530

    CoreJava_Session2Assignment1
    CoreJava_Session2Assignment1 Description

---
+++ Xpattern.java
@@ -0,0 +1,35 @@
+class Xpattern{
+
+	public static void main(String[] args)
+	{
+
+		for(int x = 0; x < 5; x ++)
+		{
+			if (x == 0)
+			{
+				System.out.println("__X_____X__");
+			}
+			else if(x == 1)
+			{
+				System.out.println("___X___X___");
+			}
+			else if(x == 2)
+			{
+				System.out.println("_____X_____");
+			}
+			else if(x == 3)
+			{
+				System.out.println("___X___X___");
+			}
+			else if(x == 4)
+			{
+				System.out.println("__X_____X__");
+			}
+			else
+			{
+				System.out.println("Invalid code");
+			}
+
+		}
+	}
+}
